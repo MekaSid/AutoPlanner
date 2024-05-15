2db3de39bf5ea8f401687d6123dfc656bf9ee528
Katy Hosokawa <hosokawa@calpoly.edu>
2024-05-15 16:43:11 -0700

    starting activity scrape

--- main.py
+++ main.py
@@ -20,18 +20,16 @@ def get_user_preferences():
 
     return user_preferences
 
-def main():
-    preferences = get_user_preferences()
-
+def hotel_scrape(preferences):
     with sync_playwright() as p:
         ss = preferences['destination']
+        ss = ss.replace(" ", "+")
         checkin = preferences['start_date']
         checkout = preferences['return_date']
         group_adults = preferences['adults']
         group_children = preferences['children']
         no_rooms = preferences['rooms']
-        ss = ss.replace(" ", "+")
-
+        
         base_url = "https://www.booking.com/searchresults.html?"
         url_params = [
             f"ss={ss}",
@@ -68,11 +66,44 @@ def main():
             if float(price) <= float(preferences['budget']):
                 hotels_list.append(hotel_dict)
 
-
         print(hotels_list)
+        
         df = pd.DataFrame(hotels_list)
 
         browser.close()
+
+def activity_scrape(preferences):
+
+    with sync_playwright() as p:
+        ss = preferences['destination']
+        ss = ss.replace(" ", "+")
+
+        browser = p.chromium.launch(headless=False)
+        activity_page = browser.new_page()
+
+        activity_url = 'https://www.tripadvisor.com/Search?q=' + ss
+        activity_page = browser.new_page()
+        activity_page.goto(activity_url, timeout = 60000)
+
+        time.sleep(3)
+        activity_page.click('button[class = "Cfwtz q wSSLS raQgL"]')
+        activity_page.wait_for_load_state('networkidle')
+
+        activities = activity_page.locator('//*[@id="lithium-root"]/main/div/div[2]/div/div/div[2]/div/div[2]/div[1]')
+
+        activities_list = []
+        for activity in activities:
+            activities_list.append(activity.locator('//*[@id="lithium-root"]/main/div/div[2]/div/div/div[2]/div/div[2]/div[1]/div[2]/div/a/div/div[2]/header/div/div/div[2]').inner_text())
+        
+        print(activities_list)
+
+        browser.close()
+
+def main():
+    preferences = get_user_preferences()
+    hotel_scrape(preferences)
+    activity_scrape(preferences)
+    
 
 
 if __name__ == '__main__':
